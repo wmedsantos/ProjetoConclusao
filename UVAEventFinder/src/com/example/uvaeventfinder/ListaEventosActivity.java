2baffe4d38c2e47709d0e19fea108143e3464dbc
20141110712 <20141110712@TJINLA06E04.LABUVA.EDU>
2016-05-21 09:55:28 -0300

    foi

---
+++ UVAEventFinder/src/com/example/uvaeventfinder/ListaEventosActivity.java
@@ -0,0 +1,17 @@
+package com.example.uvaeventfinder;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ListaEventosActivity extends Activity {
+	
+	 @Override
+	    protected void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.activity_listaeventos);
+	        
+	       
+	    }
+
+
+}
