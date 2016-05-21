1411a2a6a2bcac42bf7bfde7b86b800f4374d3be
wmedsantos <wmedsantos@hotmail.com>
2016-05-21 00:05:04 -0300

    Telas de Login e Eventos
    Somente o layout das telas

--- UVAEventFinder/gen/com/example/uvaeventfinder/R.java
+++ UVAEventFinder/gen/com/example/uvaeventfinder/R.java
@@ -11,6 +11,7 @@ public final class R {
     public static final class attr {
     }
     public static final class dimen {
+        public static final int FontGrande=0x7f040002;
         /**  Default screen margins, per the Android Design guidelines. 
 
          Example customization of dimensions originally defined in res/values/dimens.xml
@@ -22,13 +23,33 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int btnbusca=0x7f020000;
+        public static final int btnmenu=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int ic_launcher_bkp=0x7f020003;
+        public static final int logotopo=0x7f020004;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f08000f;
+        public static final int btnCriarConta=0x7f080008;
+        public static final int btnLogin=0x7f080006;
+        public static final int editText1=0x7f08000b;
+        public static final int edtLogin=0x7f080002;
+        public static final int edtSenha=0x7f080004;
+        public static final int imageButton1=0x7f080009;
+        public static final int imageView1=0x7f080000;
+        public static final int imgbtBusca=0x7f08000c;
+        public static final int linear1=0x7f08000a;
+        public static final int listView1=0x7f08000e;
+        public static final int tableRow1=0x7f080005;
+        public static final int tableRow2=0x7f080007;
+        public static final int textView1=0x7f08000d;
+        public static final int txwLogin=0x7f080001;
+        public static final int txwSenha=0x7f080003;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
+        public static final int listaeventos=0x7f030001;
     }
     public static final class menu {
         public static final int login=0x7f070000;
@@ -37,6 +58,10 @@ public final class R {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
+        public static final int proximoseventos=0x7f050003;
+        public static final int strLogin=0x7f050004;
+        public static final int strSenha=0x7f050005;
+        public static final int strlogo=0x7f050006;
     }
     public static final class style {
         /** 
