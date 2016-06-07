46247f4bbf9c26b47f3c10c98a27db51acaee465
Mauricio <Mauricio@Desktop>
2016-06-07 15:37:02 -0300

    Layoutda tela de comentarios.

---
+++ UVAEventFinder/src/com/example/uvaeventfinder/ComentariosActivity.java
@@ -0,0 +1,15 @@
+package com.example.uvaeventfinder;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class ComentariosActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_comentarios);
+	}
+}
