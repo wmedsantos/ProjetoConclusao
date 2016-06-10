e246d82f6291fdb82a3f8cc5151c4071e99c18fd
SciComputer <jc.n900@gmail.com>
2016-06-10 15:50:37 -0700

    Alterações no Schema & Inserts nas tabelas

--- UVAEventFinder/src/dao/DatabaseHelper.java
+++ UVAEventFinder/src/dao/DatabaseHelper.java
@@ -31,7 +31,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 		
 		//Tabela Público
 		db.execSQL("CREATE TABLE publico(_id INTEGER PRIMARY KEY,nome TEXT NOT NULL"+
-		"email TEXT NOT NULL,senha TEXT NOT NULL DEFAULT)");
+		"email TEXT NOT NULL,senha TEXT NOT NULL DEFAULT '123')");
 		
 		//Tabela Palestrante
 		db.execSQL("CREATE TABLE palestrante(_id INTEGER PRIMARY KEY,nome TEXT NOT NULL"+
@@ -39,32 +39,50 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 		
 		//Tabela Evento
 		db.execSQL("CREATE TABLE evento(_id INTEGER PRIMARY KEY,tema TEXT NOT NULL"+
-		"inicio NUMERIC,fim NUMERIC)");
+		"inicio TEXT,fim TEXT)");
 		
 		//Tabela Endereço
 		db.execSQL("CREATE TABLE endereco(_id INTEGER PRIMARY KEY,rua TEXT,numero TEXT"+
 		"complemento TEXT,bairro TEXT,cidade TEXT,estado TEXT,cep TEXT)");
 		
+		//Tabela Localização
+		db.execSQL("CREATE TABLE localizacao(_id INTEGER PRIMARY KEY,_id_endereco INTEGER, bloco TEXT, andar TEXT"+
+		"sala TEXT, vaga INTEGER, FOREIGN KEY (_id_local) REFERENCES endereco(_id))");
+		
 		//Tabela Sessão
 		db.execSQL("CREATE TABLE sessao(_id INTEGER PRIMARY KEY,_id_evento INTEGER NOT NULL"+
-		"_id_palestrante INTEGER NOT NULL,id_endereco INTEGER NOT NULL,tema TEXT NOT NULL"+
-				"sintese TEXT, inicio NUMERIC, fim NUMERIC, cargahora NUMERIC,imagem BLOB,"+
+		"_id_palestrante INTEGER NOT NULL,id_endereco INTEGER NOT NULL,_id_local INTEGER NOT NULL ,tema TEXT NOT NULL"+
+		"sintese TEXT, inicio TEXT, fim TEXT, cargahora TEXT,imagem BLOB,"+
 		"FOREIGN KEY(_id_evento) REFERENCES evento(_id),FOREIGN KEY(_id_palestrante) REFERENCES"+
-				"palestrante(_id),FOREIGN KEY(_id_endereco) REFERENCES endereco(_id)");
+		"palestrante(_id),FOREIGN KEY(_id_endereco) REFERENCES endereco(_id), FOREIGN KEY(_id_local)"+
+		"REFERENCES localizacao(_id)");
 		
 		//Tabela Inscrição
 		db.execSQL("CREATE TABLE inscricao(_id INTEGER PRIMARY KEY, _id_publico INTEGER NOT NULL"+
 		"_id_aluno INTEGER NOT NULL, _id_sessao INTEGER NOT NULL,data NUMERIC,FOREIGN KEY "+
-				"(_id_publico) REFERENCES publico(_id),FOREIGN KEY (_id_aluno) REFERENCES"+
+		"(_id_publico) REFERENCES publico(_id),FOREIGN KEY (_id_aluno) REFERENCES"+
 		"aluno(_id), FOREIGN KEY (_id_sessao) REFERENCES sessao(_id))");
 		
 		//Tabela opinião
 		db.execSQL("CREATE TABLE opiniao(_id INTEGER PRIMARY KEY, _id_sessao INTEGER NOT NULL"+
-		"positiva NUMERIC, sugestao TEXT, FOREIGN KEY (_id_sessao) REFERENCES sessao(_id))");
+		"positiva TEXT, sugestao TEXT, FOREIGN KEY (_id_sessao) REFERENCES sessao(_id))");
 		
-		//Tabela Localização
-		db.execSQL("CREATE TABLE localizacao(_id_local INTEGER PRIMARY KEY, bloco TEXT, andar TEXT"+
-		"sala TEXT, vaga INTEGER, FOREIGN KEY (_id_local) REFERENCES endereco(_id))");
+		
+		//Start
+		db.execSQL("INSERT INTO aluno(_matr,nome,email,curso,campus) VALUES(20011002573,'Jose Carlos'"+
+				"'jc.n900@gmail.com','Ciência da Computação','Tijuca')");
+		db.execSQL("INSERT INTO publico(_id,nome,email)VALUES(1,'Fukuda','fernando.fukuda@uva.br')");
+		db.execSQL("INSERT INTO palestrante(_id,nome,origem,cargo,observacao,email) VALUES(1,'Dra.Adriana Sicsú'"+
+		"'Universidade Veiga de Almeida','Professora','Leciona: Aplicações em Dispositivos Móveis, Monografia I e LP 1'"+
+				"'adriana.nascimento@uva.br')");
+		db.execSQL("INSERT INTO evento(_id,tema,inicio,fim) VALUES (201601,'Semana da Ciência & Tecnologia'"+
+				"'2016-06-20 09:30','2016-6-25 12:30')");
+		db.execSQL("INSERT INTO endereco(_id,rua,numero,complemento,bairro,cidade,estado,cep) VALUES"+
+				"(1,'Rua Ibituruna','108','Em frente ao metrô São Cristóvão','Tijuca','Rio de Janeiro','RJ','20.271-020')");
+		db.execSQL("INSERT INTO localizacao(_id,_id_endereco,bloco,andar,sala,vaga,) VALUES (11,1,'B','Térreo','127','6',25)");
+		db.execSQL("INSERT INTO sessao(_id,_id_evento,_id_palestrante,_id_endereco,_id_local,tema,sintese"+
+				"'inicio,fim,cargahora) VALUES (20160101,201601,1,1,11,'Programação para Dispositivos Móveis Android: Uma abrodagem prática'"+
+				"'Minicurso com enfoque em boas práticas no gerenciamento e desenvolvimento Ágil','2016-06-23 09:30','2016-06-23 11:30',4)");
 		
 	}
 	
